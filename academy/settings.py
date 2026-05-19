e973e6433bdccf1f35084c5cfd2d87d8e5f7536b
Tarun Dogra <dogratarun12@gmail.com>
2026-05-19 12:30:10 +0530

    Fixed Gmail SMTP settings

--- academy/settings.py
+++ academy/settings.py
@@ -110,9 +110,9 @@ import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
 
-EMAIL_PORT = 587
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 
 EMAIL_USE_TLS = True
 
@@ -120,6 +120,8 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 OWNER_EMAIL = os.environ.get("OWNER_EMAIL")
+
+EMAIL_TIMEOUT = 30
