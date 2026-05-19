c4ac9916ebf3d4d81fe66624cdb8015e2abf572e
Tarun Dogra <dogratarun12@gmail.com>
2026-05-19 12:46:14 +0530

    fixed gmail smtp

--- academy/settings.py
+++ academy/settings.py
@@ -113,13 +113,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
-
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'bunyacademy@gmail.com'
+EMAIL_HOST_PASSWORD = 'ghqpruievaozsiqc'
 
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
