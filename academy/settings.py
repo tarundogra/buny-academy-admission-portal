b099278de16069f112c24b5fc2710f49635535bc
Tarun Dogra <dogratarun12@gmail.com>
2026-05-23 11:00:45 +0530

    fixed smtp final

--- academy/settings.py
+++ academy/settings.py
@@ -122,18 +122,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp-relay.brevo.com'
-
-EMAIL_PORT = 587
-
-EMAIL_HOST_USER = 'aba955001@smtp-brevo.com'
-
-
 import os
 
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEFAULT_FROM_EMAIL = 'bunyacademy@gmail.com'
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 2525))
+
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
